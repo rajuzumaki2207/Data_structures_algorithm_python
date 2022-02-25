c26182977e070c022070022fd14ffd2363c5c107
Rajdeep <rajdeep.2207.shil@gmail.com>
2022-02-25 20:52:18 +0530

    Reverse_string

---
+++ Recursion/Reverse_string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: list()):
+        
+        return s[::-1]
+        
+if __name__=='__main__':
+    k=Solution()
+    print(k.reverseString(["h","e","l","l","o"]))
+
