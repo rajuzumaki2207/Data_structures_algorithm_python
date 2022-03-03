9ea78e4f223a805c86a0102fe89cc40bbfe5bee0
Rajdeep SHIL <rajdeep.shil@gmail.com>
2022-03-03 09:37:44 +0530

    Commit 2 More efficient way

--- Reverse_string_III.py
+++ Reverse_string_III.py
@@ -1,14 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        k= s[::-1].split(" ")
-        ss=str()
-        for i in reversed(range(len(k))):
-            if i!=0:
-                ss= ss+ k[i] + " "
-            else:
-                ss= ss+ k[i]
-
-        return  ss
+       
+        return  " ".join(x[::-1] for x in s.split())
+        
 if __name__=="__main__":
     k=Solution()
     print(k.reverseWords("Let's take LeetCode contest"))
