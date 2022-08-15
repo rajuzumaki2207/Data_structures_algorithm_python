4bdcfe959cbbfa21cde38a1be7614afb6f3d807f
Rajdeep SHIL <rajdeep.shil@gmail.com>
2022-08-15 13:12:58 +0530

    Updated

---
+++ Roman_to_int.py
@@ -0,0 +1,18 @@
+class Solution:
+    def __init__(self) -> None:
+        pass
+    def int_To_Roman(roman):
+        m = {'I':1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M':1000
+         }
+        
+
+        pass
+if __name__=="__main__":
+    k=Solution.int_To_Roman([0,1,2,3,4,6,7])
+    print (k)
